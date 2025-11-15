b03a725d40ad9243ef264be8f5640a900aa58ec6
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-11-15 07:30:45 +0500

    Prototype Ready without Encryption

--- Backend/Routes/Chatrooms.js
+++ Backend/Routes/Chatrooms.js
@@ -3,7 +3,8 @@ import {
     chatroomController,
     sendMessageController,
     sendGroupMessageController,
-    createGroupController
+    createGroupController,
+    addGroupMemberController
 } from "../Controllers/Chatrooms.js";
 
 const router = Router();
@@ -12,5 +13,6 @@ router.get('/', chatroomController);
 router.post('/send', sendMessageController);
 router.post('/send-group-message', sendGroupMessageController);
 router.post('/create-group', createGroupController);
+router.post('/add-group-member', addGroupMemberController);
 
 export default router;
