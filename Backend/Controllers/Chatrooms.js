41306feae3c041252df2448dd81652f92d0ffaf9
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-11-15 06:30:26 +0500

    Commit

--- Backend/Controllers/Chatrooms.js
+++ Backend/Controllers/Chatrooms.js
@@ -4,10 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const algorithm = 'aes-256-cbc';
-const secretKey = process.env.ENCRYPTION_KEY;
-const iv = crypto.randomBytes(16);
-
 export const chatroomController = async (request, response) => {
     return response.json("Hello, World! Chatrooms endpoint is working fine.");
 };
