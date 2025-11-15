c8f50d618cb1838103bf19345864adab238db327
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-11-15 07:14:49 +0500

    Testing GroupMessages

--- Backend/Routes/Chatrooms.js
+++ Backend/Routes/Chatrooms.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { chatroomController, sendMessageController } from "../Controllers/Chatrooms.js";
+import { chatroomController, sendMessageController, sendGroupMessageController } from "../Controllers/Chatrooms.js";
 
 const router = Router();
 
 router.get('/', chatroomController);
 router.post('/send', sendMessageController);
+router.post('/send-group-message', sendGroupMessageController);
 
 export default router;
