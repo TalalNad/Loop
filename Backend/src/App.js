c8e31675e11ccd522ed995e4f7c703657bf3da81
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-11-26 22:24:39 +0500

    Updated

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -3,17 +3,9 @@ import cors from 'cors';
 
 import authenticationRoutes from '../Routes/Authentication.js';
 import chatroomRoutes from '../Routes/Chatrooms.js';
-import cors from 'cors';
-
-import authenticationRoutes from '../Routes/Authentication.js';
-import chatroomRoutes from '../Routes/Chatrooms.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
-import pool from '../Config/Database.js';
-import authenticationRoutes from '../Routes/Authentication.js';
-import chatroomRoutes from '../Routes/Chatrooms.js'
 
 app.use(express.json());
 
